d14a5a903762455739d3652ce18fd832ddecc6f7
MatiasErranti <104810080+MatiasErranti@users.noreply.github.com>
2023-08-01 04:18:48 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store } from './Redux/Store/index';
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://localhost:3001'
-axios.defaults.baseURL = 'pi-pokemon-backend-production-2ebb.up.railway.app'
+axios.defaults.baseURL = 'pi-pokemon-backend-production-2ebb.up.railway.app/'
 
 // ReactDOM.render(
   // <React.StrictMode>
